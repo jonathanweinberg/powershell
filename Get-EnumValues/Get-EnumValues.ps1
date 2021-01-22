6cb6a3901fa3b30aa84c23aeda43f9867ec5f3b0
Jonathan Weinberg <jonathan.weinberg@serco-na.com>
2021-01-22 12:33:03 -0500

    Initial Commit

---
+++ Get-EnumValues/Get-EnumValues.ps1
@@ -0,0 +1,39 @@
+##Unknown Source
+
+function Get-EnumValues {  
+    <#
+   .SYNOPSIS
+       Return list of names and values for an enumeration object
+    
+   .DESCRIPTION
+       Return list of names and values for an enumeration object
+    
+   .PARAMETER Type
+       Pass in an actual type, or a string for the type name.
+
+   .EXAMPLE
+       Get-EnumValues system.dayofweek
+
+   .EXAMPLE
+       [System.DayOfWeek] | Get-EnumValues
+
+    .FUNCTIONALITY
+       General Command
+   #>
+
+    [cmdletbinding()]
+    param(
+        [parameter( Mandatory = $True,
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True)]
+        [Alias("FullName")]
+        $Type
+    )
+
+    Process {
+        [enum]::getvalues($type) |
+        Select-Object @{name = "Type"; expression = { $Type.ToString() } },
+        @{name = "Name"; expression = { $_.ToString() } },
+        @{name = "Value"; expression = { $_.value__ } }
+    }
+}
